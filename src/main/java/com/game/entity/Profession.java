a410fc9bf639f1db842e6b33828dfb02b8e74eef
AnVynn <an.vynnyk@gmail.com>
2021-05-20 00:12:47 +0300

    Change files Profession and Race

--- src/main/java/com/game/entity/Profession.java
+++ src/main/java/com/game/entity/Profession.java
@@ -1,22 +1,12 @@
 package com.game.entity;
 
 public enum Profession {
-    WARRIOR("Warrior"),
-    ROGUE("Rogue"),
-    SORCERER("Sorcerer"),
-    CLERIC("Cleric"),
-    PALADIN("Paladin"),
-    NAZGUL("Nazgul"),
-    WARLOCK("Warlock"),
-    DRUID("Druid");
-
-    private final String profession;
-
-    Profession(String profession) {
-        this.profession = profession;
-    }
-
-    public String getProfession() {
-        return profession;
-    }
+    WARRIOR,
+    ROGUE,
+    SORCERER,
+    CLERIC,
+    PALADIN,
+    NAZGUL,
+    WARLOCK,
+    DRUID
 }
--- src/main/java/com/game/entity/Race.java
+++ src/main/java/com/game/entity/Race.java
@@ -1,21 +1,11 @@
 package com.game.entity;
 
 public enum Race {
-    HUMAN("Human"),
-    DWARF("Dwarf"),
-    ELF("Elf"),
-    GIANT("Giant"),
-    ORC("Orc"),
-    TROLL("Troll"),
-    HOBBIT("Hobbit");
-
-    private final String race;
-
-    Race(String race) {
-        this.race = race;
-    }
-
-    public String getRace() {
-        return race;
-    }
+    HUMAN,
+    DWARF,
+    ELF,
+    GIANT,
+    ORC,
+    TROLL,
+    HOBBIT
 }
